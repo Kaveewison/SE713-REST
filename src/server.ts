d4e033a44ef45380ae829e022604c89dcbc5ccfa
Kaveewison <kaveewison_k@cmu.ac.th>
2026-01-13 23:07:38 +0700

    Task 8

--- src/server.ts
+++ src/server.ts
@@ -50,13 +50,13 @@ app.get("/events/:id", (req, res) => {
     }
 });  
 
-
 app.post("/events", (req, res) => {
     const newEvent: Event = req.body;
     newEvent.id = events.length + 1;
     events.push(newEvent);
     res.json(newEvent);
 });
+
 
 
 
